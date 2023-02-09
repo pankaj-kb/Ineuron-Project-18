2d3ac41d9c799643bbd4e5082d845aacc19fdd6f
pankaj-kb <pankajb2601@gmail.com>
2023-02-09 23:19:20 +0530

    added different approach for button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Task 1
 
 let navButtons = document.querySelectorAll("div:has(a)")[0];
+// let navButtons = document.querySelector("div > a.btn")
 console.log(navButtons)
 
 let psButton = document.createElement("a");
