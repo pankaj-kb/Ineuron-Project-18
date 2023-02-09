8c7d329095dbfa0f1c8158f16063028238f39ba6
pankaj-kb <pankajb2601@gmail.com>
2023-02-09 23:31:44 +0530

    task 2 complete

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let navButtons = document.querySelectorAll("div:has(a)")[0];
 // let navButtons = document.querySelector("div > a.btn")
-console.log(navButtons)
+// console.log(navButtons)
 
 let psButton = document.createElement("a");
 psButton.className="btn"
@@ -11,3 +11,12 @@ psButton.textContent = "Pro Subscription"
 navButtons.appendChild(psButton)
 
 // Task 2 adding menu item
+
+let menuList = document.querySelector(".tags-container div");
+console.log(menuList)
+
+let chineseItem = document.createElement("a");
+chineseItem.href = "#";
+chineseItem.innerText="Chinese(7)";
+
+menuList.appendChild(chineseItem);
